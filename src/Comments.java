7b4c6c4f3988b014a1b0dd576e0c675b0dbe541e
reem74 <reem.ghany70@gmail.com>
2019-03-21 22:10:29 +0200

    added Comments Class

---
+++ src/Comments.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Comments {
+    ArrayList<Token> arrayOfComments = new ArrayList<Token>();
+    private Comments() {
+        arrayOfComments.add(new Token("/*./*" ,"<MULTI_COMMENT>"));
+        arrayOfComments.add(new Token("//.\\n" ,"<SINGLE_COMMENT>"));
+    }
+    private static final Comments instance = new Comments();
+
+    public static Comments getInstance() {
+        return instance;
+    }
+    public Token arrayOfCommentsGetIdx(int idx) {
+        return arrayOfComments.get(idx);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+///First find comments in the input then find integrals then the rest
 public class Main {
     public static void main(String[] args) {
-        String input = "int main() {int reem = 0;} ;" +
+        String input = "int main() {int reem = ++;}} ;" +
                 "String found = '0'";
-        Pattern pattern = Pattern.compile(Strings.getInstance().arrayOfStringsGetIdx(1).regex);
-//        System.out.println(Strings.getInstance().arrayOfStringsGetIdx(0).regex);
+
+        Pattern pattern = Pattern.compile(Operators.getInstance().arrayOfOperatorsGetIdx(0).regex);
         Matcher matcher = pattern.matcher(input);
         while (matcher.find()) {
             System.out.println("Found a match: " + matcher.group());
