48efcc98a326995950836f129b6eae5145a05961
Abdulrahman Sobbhy <abdo@Abdulrahmans-MacBook-Pro.local>
2019-03-22 22:34:01 +0200

    add the constants regex

--- src/Constants.java
+++ src/Constants.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class Constants {
     private ArrayList<Token> arrayOfConstants = new ArrayList<Token>();
     private Constants() {
-
+        arrayOfConstants.add(new Token("[+-]?[0-9]*\\.[0-9]+","<FLOAT_LITERAL>"));
+        arrayOfConstants.add(new Token("[+-]?[0-9]+" , "<INTEGRAL_LITERAL>" ));
     }
     private static final Constants instance = new Constants();
 
